db0112d54bb936e271936d9fd746d6e722e863df
rubonics <rubenrodriguez+github@gmail.com>
2018-10-10 14:29:09 -0400

    removes firebase as a dependency
    + removes firebase as a dependency
    + updates the prototype to expect an authorized firebase, with which it can pull out the relevant site data

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
-var Firebase = require( 'firebase' );
-var objectAssign = require( 'object-assign' )
-
 module.exports = SiteData;
-
 
 /**
  * Get the data for a webhook site.
@@ -13,9 +9,7 @@ module.exports = SiteData;
  * values can be passed in as options to `get` and `set`.
  * 
  * @param {object}   opts
- * @param {object}   opts.firebase  Firebase credentials 
- * @param {object}   opts.firebase.name    Firebase name
- * @param {object}   opts.firebase.secret  Firebase secret
+ * @param {object}   opts.firebase  Root Firebase database instances
  * @param {string}   opts.siteName  The name of the webhook site
  * @param {string}   opts.key       The key of the webhook site
  * @param {Function} callback       Function called with the site data
@@ -24,14 +18,9 @@ function SiteData ( opts ) {
   if ( ! ( this instanceof SiteData ) ) return new SiteData( opts )
   if ( !opts ) opts = {}
 
-  try {
-    var firebaseName = opts.firebase.name;
-    var firebaseSecret = opts.firebase.secret;
-    var bucketsRoot = new Firebase( 'https://' + firebaseName + '.firebaseio.com/buckets' )
-  } catch ( error ) {
-    error.message = 'SiteData opts expects `name` & `secret`. The Firebase name, and secret key.'
-    return callback( error )
-  }
+  var firebaseRoot = options.firebse;
+
+  if ( ! firebaseRoot ) throw new Error( 'SiteData opts expects an authorized firebase instance.' )
 
   return {
     get: getFirebaseData,
@@ -41,19 +30,15 @@ function SiteData ( opts ) {
   function getFirebaseData ( getOpts, callback ) {
     if ( typeof getOpts === 'function' ) {
       callback = getOpts;
-      getOpts = objectAssign( {}, opts );
+      getOpts = Object.assign( {}, opts );
     }
 
-    bucketsRoot.auth( firebaseSecret, function ( error ) {
-      if ( error ) return callback( error )
+    var dataRef = firebaseRoot.ref( 'buckets' )
+      .child( escapeSiteName( getOpts.siteName ) )
+      .child( getOpts.key )
+      .child( 'dev' );
 
-      var dataRef = bucketsRoot.child( escapeSiteName( getOpts.siteName ) )
-        .child( getOpts.key )
-        .child( 'dev' );
-
-      dataRef.once( 'value', onValue, onError )
-
-    } )
+    dataRef.once( 'value', onValue, onError )
 
     function onValue ( snapshot ) { callback( null, snapshot.val() ) }
     function onError ( error ) { callback( error ) }
@@ -63,20 +48,15 @@ function SiteData ( opts ) {
   function setFirebaseData ( siteData, setOpts, callback ) {
     if ( typeof setOpts === 'function' ) {
       callback = setOpts;
-      setOpts = objectAssign( {}, opts )
+      setOpts = Object.assign( {}, opts )
     }
 
-    bucketsRoot.auth( firebaseSecret, function ( error ) {
-      if ( error ) return callback( error )
+    var dataRef = firebaseRoot.ref( 'buckets' )
+      .child( escapeSiteName( setOpts.siteName ) )
+      .child( setOpts.key )
+      .child( 'dev' );
 
-      var dataRef = bucketsRoot.child( escapeSiteName( setOpts.siteName ) )
-        .child( setOpts.key )
-        .child( 'dev' );
-
-      dataRef.set( siteData, callback )
-
-    } )
-
+    dataRef.set( siteData, callback )
   }
 
 }
